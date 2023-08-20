0e1bd6f78edda9d101984fb7ddac760a3515df43
sojinsamuel <sojinsamuel2001@gmail.com>
2023-08-20 23:58:20 +0530

    find location from the client side

--- components/ui/CalloutCard.tsx
+++ components/ui/CalloutCard.tsx
@@ -8,10 +8,28 @@ type Props = {
   location: string;
   warning?: boolean;
 };
+let CITY = "";
+
+async function getLocation() {
+  const res = await fetch(
+    `https://api.geoapify.com/v1/ipinfo?&apiKey=${process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY}`
+  );
+
+  const {
+    city: { name },
+  } = await res.json();
+  // console.log(data);
+  CITY = name;
+  return name;
+}
+
+getLocation();
 
 function CalloutCard({ location, warning }: Props) {
   const { user } = useUser();
-  const message = `Hie ${user?.firstName}, I see you are from ${location}. Below are some stats that may help you to study the climatic conditions in your area`;
+  const message = `Hie ${user?.firstName}, I see you are from ${
+    CITY || "{Searching...}"
+  }. Below are some stats that may help you to study the climatic conditions in your area`;
   return (
     <Callout
       className="mt-4 max-w-md mx-auto p-5"
